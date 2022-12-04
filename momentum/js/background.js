43aaaf483535645180cfde0c7ff442fe37b2e2e3
shinwoos <kmnj2100@gmail.com>
2022-12-05 04:48:23 +0900

    vv
    v

--- momentum/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["img0.jpg", "img1.jpg", "img2.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
